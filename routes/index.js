307f8a995f8849287baada1ed160f981e903f4bc
makenova <themarchmyth@gmail.com>
2016-11-09 22:15:52 -0600

    change logging
    use debug module to only log in development

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,32 @@
 const moment = require('moment');
+const debug = require('debug');
+const info = debug('info');
 const weather = require('../services/weather');
 
 function pastFiveMins (timestamp) {
   const lastCall = moment.unix(timestamp);
-  console.log(`lastcall was ${moment().diff(lastCall, 'minutes')} mins ago`)
+  info(`lastcall was ${moment().diff(lastCall, 'minutes')} mins ago`)
   return moment().diff(lastCall, 'minutes') >= 5;
 }
 
 function getForecast (app, req, res, next) {
-  console.log(moment().format('h:mm:ss a'),'client area request');
+  info(moment().format('h:mm:ss a'),'client area request');
 
   const latitude = req.query.lat || '35.4381759';
   const longitude = req.query.long || '-97.4529094';
   const areaKey = `${latitude},${longitude}`;
   let inMap = app.locals.areaMap[areaKey];
-  // console.log('locals', app.locals)
+  debug('extra')('locals', app.locals)
 
   if (!inMap) {
-    console.log(moment().format('h:mm:ss a'), 'respond with new request')
+    info(moment().format('h:mm:ss a'), 'respond with new request')
     weather.get(app, latitude, longitude, (err, data) => {
       if (err) return next(err);
       app.locals.areaMap[areaKey] = data;
       res.send(data);
     });
   } else if (inMap && pastFiveMins(inMap.timestamp)) {
-    console.log(moment().format('h:mm:ss a'), 'time elapsed, respond with fresh request')
+    info(moment().format('h:mm:ss a'), 'time elapsed, respond with fresh request')
     weather.get(app, latitude, longitude, (err, data) => {
       if (err) return next(err);
       app.locals.areaMap[areaKey] = data;
@@ -32,7 +34,7 @@ function getForecast (app, req, res, next) {
     });
   } else {
     // in the map and not past time
-    console.log(moment().format('h:mm:ss a'), 'respond from locals')
+    info(moment().format('h:mm:ss a'), 'respond from locals')
     res.send(app.locals.areaMap[areaKey]);
   }
 }
